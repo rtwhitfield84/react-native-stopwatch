f9772c8966e04a9cc88d7c383803a6448b4e6e51
Rico <richardthomaswhitfield@gmail.com>
2017-03-04 16:10:43 -0600

    initial app setup

---
+++ stopwatch/index.ios.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  View
+} from 'react-native';
+
+
+var Stopwatch = React.createClass({
+  render: function() {
+
+  } 
+});
+
+AppRegistry.registerComponent('stopwatch', () => Stopwatch);
+
